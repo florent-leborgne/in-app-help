336d93f171099e6000546e9f9af223a788e05a0e
florent-leborgne <florent.leborgne@elastic.co>
2022-07-25 17:22:51 +0200

    imports

--- src/pages/current/index.tsx
+++ src/pages/current/index.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react';
 import { EuiFlexGroup, EuiFlexItem, EuiCard, EuiIcon } from '@elastic/eui';
 import KibanaLayout from '../../layouts/kibana';
+import React from 'react';
 
 const Index: FunctionComponent = () => {
   return (
--- src/pages/current/passive.tsx
+++ src/pages/current/passive.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react';
 import Link from 'next/link';
 import { EuiLink, EuiText } from '@elastic/eui';
 import KibanaLayout from '../../layouts/kibana';
+import React from 'react';
 
 const Discover: FunctionComponent = () => {
   return (
